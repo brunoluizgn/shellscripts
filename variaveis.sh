4457a8635929334138a256599ed6f7d6b6e972e6
brunoluizgn <brunoluiz.gn@gmail.com>
2023-05-16 19:42:38 -0300

    first commit

---
+++ variaveis.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+
+NOME= "BrunoLuiz"
+
+echo "BrunoLuiz"
+
+NUMERO_1=25
+NUMERO_2=25
+TOTAL= $(($NUMERO_1+$NUMERO_2))
+
+echo "$TOTAL"
+
+SAIDA_CAT= "$(cat /etc/passwd | grep bruno)"
+
+echo "SAIDA_CAT"
+
+echo "*---------------------------*"
+
+echo "parametro 1: $1"
+echo "parametro 2: $2"
+
+echo "Todos os parametros: $*"
+
+echo "Quantos parametros enviados? $#"
+echo "Saida do ultimo comando: $?"
+echo "PID: $$"
+echo "$0" 
