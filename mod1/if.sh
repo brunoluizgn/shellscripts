6a741db6ed94a3cfc0bd6bc9c6c766ee86f51055
brunoluizgn <brunoluiz.gn@gmail.com>
2023-07-08 23:43:05 -0300

    voltar ao curso de shell

---
+++ mod1/if.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+VAR="1"
+VAR2="1"
+
+if [[ "$VAR" = "$VAR2" ]]; then 
+    echo "São iguais"
+fi
+
+if [[ "$VAR" = "$VAR2" ]]
+then 
+    echo "São iguais"
+fi
+
+if test "$VAR" = "$VAR2"
+then 
+    echo "São iguais"
+fi
+
+if [ "$VAR" = "$VAR2" ]
+then 
+    echo "São iguais"
+fi
--- mod1/variaveis.sh
+++ mod1/variaveis.sh
@@ -1,27 +1,22 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 
-NOME= "BrunoLuiz"
-
-echo "BrunoLuiz"
+NOME= "Bruno Luiz"
+echo $NOME
 
 NUMERO_1=25
 NUMERO_2=25
 TOTAL= $(($NUMERO_1+$NUMERO_2))
-
 echo "$TOTAL"
 
-SAIDA_CAT= "$(cat /etc/passwd | grep bruno)"
+SAIDA_CAT=$(cat /etc/passwd | grep bruno)
+echo "$SAIDA_CAT"
 
-echo "SAIDA_CAT"
-
-echo "*-------------------------*"
+echo "*------------NOVA_AULA-------------*"
 
 echo "parametro 1: $1"
 echo "parametro 2: $2"
-
 echo "Todos os parametros: $*"
-
 echo "Quantos parametros enviados? $#"
 echo "Saida do ultimo comando: $?"
 echo "PID: $$"
