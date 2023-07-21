d9475bf2014f46f06cd03596cc63a0bc8fcf3c78
brunoluizgn <brunoluiz.gn@gmail.com>
2023-07-20 21:50:16 -0300

    debug e cores

---
+++ mod4/debug_1.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+#listarUsuarios.sh - Extrai usuários do /etc/passwd
+
+#Site: https://borenore.com.br
+#Autor: Bruno Luiz
+#Manutenção: Bruno Luiz
+#--------------------------------------------------------------------------------------------#
+#Irá extrar  usuários do /etc/passwd, havendo a possibilidade de colocar em maiúsculo e 
+#em ordem alfabética
+#
+#Exemplo:
+#   $ ./listarUsuarios.sh -s -m
+#   Neste exemplo ficará em maiúscula e em ordem alfabética 
+#----------------------------------------------------------------------------------------------#
+#Histórico:
+#   
+#   v1.1 17/07/2023, Bruno Luiz:
+#   -  adicionado -s, -h, -v
+#   -  adicionado basename
+#   v1.2 17/07/2023. Bruno Luiz:
+#   -  case
+#   - chave valor
+#   v1.3 17/07/2023 Bruno Luiz:
+#   - Adicionado while com shift e teste de variável 
+#   - Adicionado 2 flags
+#----------------------------------------------------------------------------------------------#
+#Testadi en:
+#   GNU bash, versão 5.0.17(1)-release (x86_64-pc-linux-gnu)
+#
+#----------------------------------------------------------------------------------------------#
+
+#----------------------------------VARIAVEIS----------------------------------------------------#
+USUARIOS="$(cat /etc/passwd | cut -d : -f 1)"
+MENSAGEM_USO="
+$(basename $0) - [OPÇÕES]
+
+ -h - Menu de ajuda
+ -v - Versão
+ -s - Ordernar a saída
+ -m - Coloca em maiúsculo
+"
+VERSAO="V1.1"
+CHAVE_ORDENA=0
+CHAVE_MAIUSCULO=0
+
+#----------------------------------------EXECUÇÃO------------------------------------------------------#
+
+while test -n "$1"
+do
+    case "$1" in
+        -h) echo "$MENSAGEM_USO" && exit 0                ;; 
+        -v) echo "$VERSAO" && exit 0                      ;;
+        -s) CHAVE_ORDENA=1                                ;;
+        -m) CHAVE_MAIUSCULO=1                             ;;
+         *) echo "Opção inválida, valide o -h." && exit 1 ;;
+    esac
+    shift
+done     
+set -x
+[ $CHAVE_ORDENA -eq 1 ]     && USUARIOS=$(echo "$USUARIOS" | sort)
+[ $CHAVE_MAIUSCULO -eq 1 ]  && USUARIOS=$(echo "$USUARIOS" | tr [a-z] [A-Z])
+set +x
+echo "$USUARIOS"  
