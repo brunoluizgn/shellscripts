24ee0f9e27707d6bc78aaabd852419d1b6981b6b
brunoluizgn <brunoluiz.gn@gmail.com>
2023-07-24 20:07:25 -0300

    sistema grafico

---
+++ mod10/sistema_grafico.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+#
+# sistema_de_usuarios.sh - Sistema para gerenciamento de usuários
+#
+# Site:       https://4fasters.com.br
+# Autor:      Mateus Gabriel
+# Manutenção: Mateus Gabriel
+#
+# ------------------------------------------------------------------------ #
+#  Este programa faz todas as funções de gerenciamento de usuários, como:
+#  inserir, deletar, alterar.
+#
+#  Exemplos:
+#      $ source sistema_de_usuarios.sh
+#      $ ListaUsuarios
+# ------------------------------------------------------------------------ #
+# Histórico:
+#
+#   v1.0 06/10/2018, Mateus:
+#       - Tratamento de erros com relação ao arquivo do banco de dados
+#       - Lista usuários
+# ------------------------------------------------------------------------ #
+# Testado em:
+#   bash 4.4.19
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- VARIÁVEIS ----------------------------------------- #
+ARQUIVO_BANCO_DE_DADOS="banco_de_dados.txt"
+SEP=:
+TEMP=temp.$$
+VERDE="\033[32;1m"
+VERMELHO="\033[31;1m"
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- TESTES ----------------------------------------- #
+[ ! -e "$ARQUIVO_BANCO_DE_DADOS" ] && echo "ERRO. Arquivo não existe." && exit 1
+[ ! -r "$ARQUIVO_BANCO_DE_DADOS" ] && echo "ERRO. Arquivo não tem permissão de leitura." && exit 1
+[ ! -w "$ARQUIVO_BANCO_DE_DADOS" ] && echo "ERRO. Arquivo não tem permissão de escrita." && exit 1
+[ ! -x "$(which dialog)" ]         && sudo apt install dialog 1> /dev/null 2>&1
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- FUNÇÕES ----------------------------------------- #
+
+ListaUsuarios () {
+  egrep -v "^#|^$" "$ARQUIVO_BANCO_DE_DADOS" | tr : ' ' > "$TEMP"
+  dialog --title "Lista de Usuários" --textbox "$TEMP" 20 40
+  rm -f "$TEMP" 
+}
+
+ValidaExistenciaUsuario () {
+  grep -i -q "$1$SEP" "$ARQUIVO_BANCO_DE_DADOS"
+}
+
+InsereUsuario () {
+  local nome="$(echo $1 | cut -d $SEP -f 2)"
+
+  if ValidaExistenciaUsuario "$nome"
+  then
+    echo "ERRO. Usuário já existente!"
+  else
+    echo "$*" >> "$ARQUIVO_BANCO_DE_DADOS"
+    echo "Usuário cadastrado com sucesso!"
+  fi
+  OrdenaLista
+}
+
+ApagaUsuario () {
+  ValidaExistenciaUsuario "$1" || return
+
+  grep -i -v "$1$SEP" "$ARQUIVO_BANCO_DE_DADOS" > "$TEMP"
+  mv "$TEMP" "$ARQUIVO_BANCO_DE_DADOS"
+
+  echo "Usuário removido com sucesso!"
+  OrdenaLista
+}
+
+OrdenaLista () {
+  sort "$ARQUIVO_BANCO_DE_DADOS" > "$TEMP"
+  mv "$TEMP" "$ARQUIVO_BANCO_DE_DADOS"
+}
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- EXECUÇÃO ----------------------------------------- #
+
+# ------------------------------------------------------------------------ #
